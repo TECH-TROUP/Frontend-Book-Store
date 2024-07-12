2b3c5ed66aa3543cb704288e1e28314608f00e76
Ludhiya <ludhiyargiji1@gmail.com>
2024-07-12 12:56:29 +0800

    onSubmit added

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -4,6 +4,8 @@ import Logo from "../images/logo.jpg";
 import CustomInput from "../components/CustomInput";
 
 export default function Register() {
+  const registerUser = () => {};
+
   return (
     <div
       className="h-screen bg-cover bg-center flex justify-center items-center brightness-75"
@@ -12,13 +14,13 @@ export default function Register() {
       <div className="bg-white p-8 rounded-lg flex flex-col justify-center items-center w-1/4">
         <img src={Logo} alt="Logo" className="rounded-full w-24 h-24" />
         <div className="my-6 text-2xl font-bold">SIGN UP</div>
-        <form>
+        <form onSubmit={registerUser}>
           <CustomInput placeholder="Name" type="text" />
           <CustomInput placeholder="Email" type="email" />
           <CustomInput placeholder="Username" type="text" />
           <CustomInput placeholder="Password" type="password" />
           <button
-            type="button"
+            type="submit"
             className="bg-zinc-700 rounded-full px-4 py-2 text-white mt-7"
           >
             Create an account
