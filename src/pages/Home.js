ea7c4889b5d3b51286b0c54030d6cea304c6001c
Ludhiya <ludhiyargiji1@gmail.com>
2024-08-01 22:14:19 +0800

    update home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { useUserContext } from "../context/userContext";
-import NavBar from "../components/NavBar";
 
 export default function Home() {
   const { user, logout } = useUserContext();
-  return (
-    <div>
-      Home
-      <button onClick={logout}>Logout</button>
-    </div>
+  return user ? (
+    <div className="text-white text-3xl">Welcome {user.name}</div>
+  ) : (
+    <div></div>
   );
 }
