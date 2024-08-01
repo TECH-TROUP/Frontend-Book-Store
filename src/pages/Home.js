88638f7b810d784baeea548b2f8e7846998a0b4d
Ludhiya <ludhiyargiji1@gmail.com>
2024-08-01 22:10:56 +0800

    add home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,13 @@
 import React from "react";
+import { useUserContext } from "../context/userContext";
+import NavBar from "../components/NavBar";
 
 export default function Home() {
-  return <div>home</div>;
+  const { user, logout } = useUserContext();
+  return (
+    <div>
+      Home
+      <button onClick={logout}>Logout</button>
+    </div>
+  );
 }
