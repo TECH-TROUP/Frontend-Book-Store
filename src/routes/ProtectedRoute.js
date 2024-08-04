ce60323649c754f2c972e7c8ddd44f6345954dea
Ludhiya <ludhiyargiji1@gmail.com>
2024-08-04 10:59:24 +0800

    -

--- src/routes/ProtectedRoute.js
+++ src/routes/ProtectedRoute.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { Navigate } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import { useUserContext } from "../context/userContext";
 
-const ProtectedRoute = ({ children }) => {
+const ProtectedRoute = () => {
   const { user } = useUserContext();
 
   if (!user) {
     return <Navigate to="/login" />;
   }
 
-  return children;
+  return <Outlet />;
 };
 
 export default ProtectedRoute;
