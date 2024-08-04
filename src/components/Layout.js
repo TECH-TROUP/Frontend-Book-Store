7c24422ea230e31eabeed5c27e61269321fb6af2
Ludhiya <ludhiyargiji1@gmail.com>
2024-08-04 10:58:36 +0800

    update layout

---
+++ src/components/Layout.js
@@ -0,0 +1,21 @@
+import { useLocation } from "react-router-dom";
+import NavBar from "./NavBar";
+
+const Layout = ({ children }) => {
+  const { pathname } = useLocation();
+
+  const shouldApplyPadding = pathname !== "/login" && pathname !== "/register";
+
+  return (
+    <div
+      className={`flex flex-col h-screen bg-gradient-to-r from-purple-950 to-black space-y-6 ${
+        shouldApplyPadding ? "p-8" : ""
+      } font-poiret`}
+    >
+      {shouldApplyPadding && <NavBar />}
+      <div className="h-screen">{children}</div>
+    </div>
+  );
+};
+
+export default Layout;
