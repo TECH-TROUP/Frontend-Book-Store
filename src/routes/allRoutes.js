445db9ac34ebe4adb50db58ac62a851edb76351f
Ludhiya <ludhiyargiji1@gmail.com>
2024-08-04 10:59:20 +0800

    clean up

--- src/routes/allRoutes.js
+++ src/routes/allRoutes.js
@@ -1,43 +1,26 @@
 import React from "react";
 import {
   Navigate,
+  Outlet,
   Route,
   BrowserRouter as Router,
   Routes,
-  useLocation,
 } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import { UserProvider, useUserContext } from "../context/userContext";
-import ProtectedRoute from "./ProtectedRoute";
-import NavBar from "../components/NavBar";
+// import ProtectedRoute from "./ProtectedRoute";
+import Layout from "../components/Layout";
 
-const RedirectToHomeIfAuthenticated = ({ children }) => {
+const RedirectToHomeIfAuthenticated = () => {
   const { user } = useUserContext();
 
   if (user) {
     return <Navigate to="/" />;
   }
 
-  return children;
-};
-
-const Layout = ({ children }) => {
-  const { pathname } = useLocation();
-
-  const shouldApplyPadding = pathname !== "/login" && pathname !== "/register";
-
-  return (
-    <div
-      className={`flex flex-col h-screen bg-gradient-to-r from-purple-950 to-black space-y-6 ${
-        shouldApplyPadding ? "p-8" : ""
-      } font-poiret`}
-    >
-      {shouldApplyPadding && <NavBar />}
-      <div className="h-screen">{children}</div>
-    </div>
-  );
+  return <Outlet />;
 };
 
 export default function AllRoutes() {
@@ -53,28 +36,16 @@ export default function AllRoutes() {
                   <Route path="/" element={<Home />} />
                   <Route
                     path="/login"
-                    element={
-                      <RedirectToHomeIfAuthenticated>
-                        <Login />
-                      </RedirectToHomeIfAuthenticated>
-                    }
-                  />
+                    element={<RedirectToHomeIfAuthenticated />}
+                  >
+                    <Route path="/login" element={<Login />} />
+                  </Route>
                   <Route
                     path="/register"
-                    element={
-                      <RedirectToHomeIfAuthenticated>
-                        <Register />
-                      </RedirectToHomeIfAuthenticated>
-                    }
-                  />
-                  {/* <Route
-                    path="/protected"
-                    element={
-                      <ProtectedRoute>
-                        <ProtectedComponent />
-                      </ProtectedRoute>
-                    }
-                  /> */}
+                    element={<RedirectToHomeIfAuthenticated />}
+                  >
+                    <Route path="/register" element={<Register />} />
+                  </Route>
                 </Routes>
               </Layout>
             }
