69e1f086148deccc88cfe4f71b3b1587cf4954af
Ludhiya <ludhiyargiji1@gmail.com>
2024-08-04 10:58:58 +0800

    -

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useUserContext } from "../context/userContext";
 
 export default function Home() {
-  const { user, logout } = useUserContext();
+  const { user } = useUserContext();
   return user ? (
     <div className="text-white text-3xl">Welcome {user.name}</div>
   ) : (
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Login_bg from "../assets/images/login_bg.jpg";
 import Logo from "../assets/images/logo.png";
 import CustomInput from "../components/CustomInput";
