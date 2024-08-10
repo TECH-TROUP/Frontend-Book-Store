f3efecf03ec13e736a3559ce7a43fcb7886c779f
Ludhiya <ludhiyargiji1@gmail.com>
2024-08-10 23:32:58 +0800

    added cart and wishlist context

---
+++ src/context/cartContext.js
@@ -0,0 +1,82 @@
+import axios from "axios";
+import { createContext, useContext, useEffect, useState } from "react";
+import authService from "../authentication/authService";
+import { useUserContext } from "./userContext";
+
+const CartContext = createContext();
+
+export function useCartContext() {
+  return useContext(CartContext);
+}
+
+export const CartProvider = ({ children }) => {
+  // State
+  const [loading, setLoading] = useState(true);
+  const [cart, setCart] = useState([]);
+
+  //context
+  const { user } = useUserContext();
+
+  useEffect(() => {
+    if (user) {
+      fetchCart();
+    } else {
+      setLoading(false);
+    }
+  }, [user]);
+
+  const fetchCart = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:3000/api/cart/book-ids",
+        authService.getAuthHeader()
+      );
+      setCart(response.data.bookIds);
+      setLoading(false);
+    } catch (error) {
+      console.log("Error fetching cart:", error.response.data.error);
+    }
+  };
+
+  const addToCart = async (bookId, quantity, type) => {
+    try {
+      const response = await axios.post(
+        `http://localhost:3000/api/cart/add`,
+        { bookId, quantity, type },
+        authService.getAuthHeader()
+      );
+      console.log("Book added to cart:", response.data);
+      await fetchCart();
+      return response.data;
+    } catch (error) {
+      console.log("Error adding book to cart:", error.response.data.error);
+    }
+  };
+
+  const removeFromCart = async (bookId) => {
+    try {
+      const response = await axios.post(
+        `http://localhost:3000/api/cart/remove`,
+        { bookId },
+        authService.getAuthHeader()
+      );
+      console.log("Book removed from cart:", response.data);
+      await fetchCart();
+      return response.data;
+    } catch (error) {
+      console.log("Error adding book to cart:", error.response.data.error);
+    }
+  };
+
+  const value = {
+    cart,
+    addToCart,
+    removeFromCart,
+  };
+
+  return (
+    <CartContext.Provider value={value}>
+      {!loading && children}
+    </CartContext.Provider>
+  );
+};
---
+++ src/context/wishlistContext.js
@@ -0,0 +1,82 @@
+import axios from "axios";
+import { createContext, useContext, useEffect, useState } from "react";
+import authService from "../authentication/authService";
+import { useUserContext } from "./userContext";
+
+const WishlistContext = createContext();
+
+export function useWishlistContext() {
+  return useContext(WishlistContext);
+}
+
+export const WishlistProvider = ({ children }) => {
+  // State
+  const [loading, setLoading] = useState(true);
+  const [wishlist, setWishlist] = useState([]);
+
+  //context
+  const { user } = useUserContext();
+
+  useEffect(() => {
+    if (user) {
+      fetchWishlist();
+    } else {
+      setLoading(false);
+    }
+  }, [user]);
+
+  const fetchWishlist = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:3000/api/wishlist/book-ids",
+        authService.getAuthHeader()
+      );
+      setWishlist(response.data.bookIds);
+      setLoading(false);
+    } catch (error) {
+      console.log("Error fetching wishlist:", error.response.data.error);
+    }
+  };
+
+  const addToWishlist = async (bookId) => {
+    try {
+      const response = await axios.post(
+        `http://localhost:3000/api/wishlist/add`,
+        { bookId },
+        authService.getAuthHeader()
+      );
+      console.log("Book added to wishlist:", response.data);
+      await fetchWishlist();
+      return response.data;
+    } catch (error) {
+      console.log("Error adding book to wishlist:", error.response.data.error);
+    }
+  };
+
+  const removeFromWishlist = async (bookId) => {
+    try {
+      const response = await axios.post(
+        `http://localhost:3000/api/wishlist/remove`,
+        { bookId },
+        authService.getAuthHeader()
+      );
+      console.log("Book removed from wishlist:", response.data);
+      await fetchWishlist();
+      return response.data;
+    } catch (error) {
+      console.log("Error adding book to wishlist:", error.response.data.error);
+    }
+  };
+
+  const value = {
+    wishlist,
+    addToWishlist,
+    removeFromWishlist,
+  };
+
+  return (
+    <WishlistContext.Provider value={value}>
+      {!loading && children}
+    </WishlistContext.Provider>
+  );
+};
