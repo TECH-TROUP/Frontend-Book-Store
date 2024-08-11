65501fcdb3336191deb7bb13ff4794c2a016d2f5
Ludhiya <ludhiyargiji1@gmail.com>
2024-08-11 14:01:38 +0800

    added update quantiy function

--- src/context/cartContext.js
+++ src/context/cartContext.js
@@ -68,11 +68,38 @@ export const CartProvider = ({ children }) => {
       console.log("Error adding book to cart:", error.response.data.error);
     }
   };
+  const updateQuantityInCart = async (bookId, quantity) => {
+    try {
+      const response = await axios.post(
+        `http://localhost:3000/api/cart/update-quantity`,
+        { bookId, quantity },
+        authService.getAuthHeader()
+      );
+      console.log("Quantity updated in cart:", response.data);
+      return response.data;
+    } catch (error) {
+      console.log("Error updating quantity cart:", error.response.data.error);
+    }
+  };
+
+  const fetchUserCart = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:3000/api/cart/user",
+        authService.getAuthHeader()
+      );
+      return response.data;
+    } catch (error) {
+      console.log("Error fetching cart:", error.response.data.error);
+    }
+  };
 
   const value = {
     cart,
     addToCart,
     removeFromCart,
+    updateQuantityInCart,
+    fetchUserCart,
   };
 
   return (
