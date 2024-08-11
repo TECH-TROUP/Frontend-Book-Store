431697624d3d41621cb3d24e3b4558628ec5e45c
Ludhiya <ludhiyargiji1@gmail.com>
2024-08-12 01:33:07 +0800

    -

--- src/pages/customer/components/ReviewsSection.js
+++ src/pages/customer/components/ReviewsSection.js
@@ -21,6 +21,7 @@ export default function ReviewsSection({ bookId, averageRating, reviewCount }) {
   //   Initialize Data
   useEffect(() => {
     fetchReviews();
+    // eslint-disable-next-line
   }, []);
 
   const fetchReviews = async () => {
@@ -97,50 +98,54 @@ export default function ReviewsSection({ bookId, averageRating, reviewCount }) {
 
   return (
     <>
-      <div className="flex w-full space-x-4 items-start">
-        <div className="w-1/4 bg-blue-800/50 rounded-lg flex flex-col items-start py-2 px-4">
-          <div className="text-lg font-bold">Customer Reviews</div>
-          <div className="flex items-center space-x-2 mt-2">
-            {renderStars(averageRating, false)}
-            <span className="text-sm">{averageRating.toFixed(1)}</span>
-          </div>
-          <div className="text-xl text-left">{reviewCount} Reviews</div>
-        </div>
-        <div className="w-3/4 flex flex-col items-start max-h-[calc(100vh-150px)] space-y-4">
-          <div className="flex w-full space-x-4 items-center">
-            <textarea
-              rows={3}
-              placeholder="Write your review"
-              value={comment}
-              onChange={handleCommentChange}
-              className="outline-none p-2 rounded-lg text-black w-full"
-            />
-            <div className="flex space-x-2 bg-white text-black rounded-lg p-2">
-              {renderStars(rating, true)}
+      {loading ? (
+        <div></div>
+      ) : (
+        <div className="flex w-full space-x-4 items-start">
+          <div className="w-1/4 bg-blue-800/50 rounded-lg flex flex-col items-start py-2 px-4">
+            <div className="text-lg font-bold">Customer Reviews</div>
+            <div className="flex items-center space-x-2 mt-2">
+              {renderStars(averageRating, false)}
+              <span className="text-sm">{averageRating.toFixed(1)}</span>
             </div>
-            <button
-              onClick={handleSubmit}
-              className="bg-green-600 rounded-lg py-2 px-4 hover:bg-green-800 transition-color duration-300"
-              disabled={submitting}
-            >
-              {submitting ? "Submitting..." : "Submit"}
-            </button>
+            <div className="text-xl text-left">{reviewCount} Reviews</div>
           </div>
-          <div className=" flex flex-col space-y-4 flex-1 overflow-y-auto w-full">
-            {reviews.map((review, idx) => (
-              <div
-                key={idx}
-                className="flex flex-col space-y-2 items-start bg-blue-800 rounded-lg px-4 py-2 w-full"
-              >
-                <ReviewTitle review={review} />
-                <div className="text-sm bg-blue-600/50 rounded-lg px-4 py-2 text-left">
-                  {review.comment}
-                </div>
+          <div className="w-3/4 flex flex-col items-start max-h-[calc(100vh-150px)] space-y-4">
+            <div className="flex w-full space-x-4 items-center">
+              <textarea
+                rows={3}
+                placeholder="Write your review"
+                value={comment}
+                onChange={handleCommentChange}
+                className="outline-none p-2 rounded-lg text-black w-full"
+              />
+              <div className="flex space-x-2 bg-white text-black rounded-lg p-2">
+                {renderStars(rating, true)}
               </div>
-            ))}
+              <button
+                onClick={handleSubmit}
+                className="bg-green-600 rounded-lg py-2 px-4 hover:bg-green-800 transition-color duration-300"
+                disabled={submitting}
+              >
+                {submitting ? "Submitting..." : "Submit"}
+              </button>
+            </div>
+            <div className=" flex flex-col space-y-4 flex-1 overflow-y-auto w-full">
+              {reviews.map((review, idx) => (
+                <div
+                  key={idx}
+                  className="flex flex-col space-y-2 items-start bg-blue-800 rounded-lg px-4 py-2 w-full"
+                >
+                  <ReviewTitle review={review} />
+                  <div className="text-sm bg-blue-600/50 rounded-lg px-4 py-2 text-left">
+                    {review.comment}
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
+      )}
       <Snackbar
         data={snackbarMessage}
         isVisible={isSnackbarVisible}
