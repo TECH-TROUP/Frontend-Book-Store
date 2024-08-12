ff8da4e16cc0b2c24eab18a0d7773cf11edd7f8a
Ludhiya <ludhiyargiji1@gmail.com>
2024-08-12 15:53:34 +0800

    -

--- src/routes/allRoutes.js
+++ src/routes/allRoutes.js
@@ -61,6 +61,7 @@ export default function AllRoutes() {
                 <OrderProvider>
                   <Elements stripe={stripePromise}>
                     <Routes>
+                      <Route path="/" element={<Navigate to="/home" />} />
                       <Route
                         path="/*"
                         element={
